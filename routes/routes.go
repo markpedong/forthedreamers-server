e1bd18151ef297b5b99537f4b3991c4ce534ae56
markpedong <mark.pedong01@gmail.com>
2024-07-25 21:24:01 +0800

    no message

--- routes/routes.go
+++ routes/routes.go
@@ -1,12 +1,22 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/forthedreamers-server/controllers"
 	"github.com/forthedreamers-server/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateRoutes(r *gin.Engine) {
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "API IS WORKING",
+			"success": true,
+			"status":  http.StatusOK,
+		})
+	})
+
 	public := r.Group("/public")
 	{
 		public.POST("/login", controllers.Login)
