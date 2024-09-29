25c7bc3581657cff5671174e856d7e0adea1d382
markpedong <mark.pedong01@gmail.com>
2024-09-29 15:24:04 +0800

    remove cors.

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/forthedreamers-server/cloudinary"
 	"github.com/forthedreamers-server/database"
 	"github.com/forthedreamers-server/routes"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
 )
@@ -20,16 +19,16 @@ func main() {
 	r := gin.New()
 
 	r.Use(gin.Logger())
-	r.Use(cors.New(cors.Config{
-		AllowOrigins: []string{
-			"https://forthedreamers-admin.vercel.app",
-			"https://forthedreamers.vercel.app",
-		},
-		AllowMethods:     []string{"POST", "GET"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length", "Content-Type"},
-		AllowCredentials: true,
-	}))
+	// r.Use(cors.New(cors.Config{
+	// 	AllowOrigins: []string{
+	// 		"https://forthedreamers-admin.vercel.app",
+	// 		"https://forthedreamers.vercel.app",
+	// 	},
+	// 	AllowMethods:     []string{"POST", "GET"},
+	// 	AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+	// 	ExposeHeaders:    []string{"Content-Length", "Content-Type"},
+	// 	AllowCredentials: true,
+	// }))
 
 	r.MaxMultipartMemory = 20 << 20
 	routes.CreateRoutes(r)
